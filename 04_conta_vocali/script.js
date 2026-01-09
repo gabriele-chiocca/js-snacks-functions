bf31ce6d5e8dd026cbfd2df4f50efd5a23428f6d
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-09 15:51:43 +0100

    structure 4 snack

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,12 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e
+restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
+const vocali = ['a', 'e', 'i', 'o', 'u'];
 
 // Dichiara la funzione qui.
 
-
 // Invoca la funzione qui e stampa il risultato in console
-
-
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
