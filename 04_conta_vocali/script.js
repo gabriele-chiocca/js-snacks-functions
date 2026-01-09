cc883497e183e23e649f51fa37383f11f7eafaad
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-09 16:17:21 +0100

    4 snack complete

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -3,21 +3,27 @@ restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
-const vocali = ['a', 'e', 'i', 'o', 'u'];
-
-const insertVocals = '';
-
 // Dichiara la funzione qui.
 
 function verifyVocals(string) {
+  let countVoc = 0;
   for (let i = 0; i < string.length; i++) {
-    const checkVocals = string.length[i];
+    const checkVocals = string[i];
 
-    if (checkVocals === vocali) {
+    if (vocali.includes(checkVocals)) {
+      countVoc++;
+      insertVocals.push(checkVocals);
     }
   }
+  console.log(`${countVoc} (${insertVocals.join()})`);
 }
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const vocali = ['a', 'e', 'i', 'o', 'u'];
+
+const insertVocals = [];
+
+verifyVocals(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
